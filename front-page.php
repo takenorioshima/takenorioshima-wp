59eb979490aaf4c6bcbef13f8c109992b30e4396
Takenori Oshima <takenorioshima@gmail.com>
2022-11-15 15:56:15 +0900

    update: Separate hero-unit template

--- front-page.php
+++ front-page.php
@@ -10,42 +10,8 @@
 get_header();
 ?>
 
-<div id="hero-unit" class="hero-unit d-flex align-items-center justify-content-center">
-	<div class="container">
-		<h1 class="text-center fst-italic">Takenori Oshima</h1>
-		<div class="row">
-			<div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-				<p class="my-3">
-					ギタリスト・歌うたい・コンポーザ・プログラマ・デザイナー。ソフトウェアとハードウェアの狭間を際限なく行き来する自由工作員として奔走する傍ら、Senkawos／タとエ／大島武宜と働く機械などで演奏活動を行なっている。好きな猛禽類はフクロウ。
-				</p>
-			</div>
-		</div>
-		<ul class="list-unstyled text-center list-inline">
-			<li class="list-inline-item px-2"><a class="text-black text-decoration-none" href="https://twitter.com/takenorioshima"><i class="bi-twitter fs-3"></i></a></li>
-			<li class="list-inline-item px-2"><a class="text-black text-decoration-none" href="https://fb.me/takenorioshima"><i class="bi-facebook fs-3"></i></a></li>
-			<li class="list-inline-item px-2"><a class="text-black text-decoration-none" href="https://github.com/takenorioshima"><i class="bi-github fs-3"></i></a></li>
-		</ul>
+<?php get_template_part( 'section-templates/hero-unit' ); ?>
 
-		<div class="hero-unit-listen row text-center">
-			<div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3 border-top border-dark pt-3 mt-3 pt-md-5 mt-md-5">
-				<div class="hero-unit-listen-item my-2">
-					<h5 class="d-inline-block me-3">Senkawos</h4>
-					<a class="d-inline-block me-2 text-black text-decoration-none" href="https://music.apple.com/us/artist/senkawos/346967149"><i class="bi-apple"></i> Apple Music</a>
-					<a class="d-inline-block me-2 text-black text-decoration-none" href="https://open.spotify.com/artist/3h39cJNTxzSNAt9sOtWbea"><i class="bi-spotify"></i> Spotify</a>
-				</div>
-				<div class="hero-unit-listen-item my-2">
-					<h5 class="d-inline-block me-3">タとエ</h4>
-					<a class="d-inline-block me-2 text-black text-decoration-none" href="https://music.apple.com/us/artist/%E3%82%BF%E3%81%A8%E3%82%A8/1446892176"><i class="bi-apple"></i> Apple Music</a>
-					<a class="d-inline-block me-2 text-black text-decoration-none" href="https://open.spotify.com/artist/5DjSU04aYwP1yj9z7V0xrB"><i class="bi-spotify"></i> Spotify</a>
-				</div>
-				<div class="hero-unit-listen-item my-2">
-					<h5 class="d-inline-block me-3">大島武宜と働く機械</h4>
-					<a class="d-inline-block me-2 text-black text-decoration-none" href="https://www.youtube.com/channel/UCgZpOr9iVpn4sxV78ZfJs-g"><i class="bi-youtube"></i> YouTube</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div><!-- #hero-unit -->
 
 <?php
 get_footer();
