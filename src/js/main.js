1db66dc92a2d780871d7a84414b4b534385f1fe3
Takenori Oshima <takenorioshima@gmail.com>
2022-11-19 12:25:54 +0900

    clean: Remove test script

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 import "bootstrap";
 
 // Add your custom JS here.
-document.addEventListener("DOMContentLoaded", () => {
-  alert("DOM Ready.");
-});
+document.addEventListener("DOMContentLoaded", () => {});
