f8d469e6f1d82508e8038c963eab5eaff3c982f8
Takenori Oshima <takenorioshima@gmail.com>
2022-11-22 17:40:59 +0900

    update: Add icon to headline

--- section-templates/recent-posts.php
+++ section-templates/recent-posts.php
@@ -1,5 +1,5 @@
 <div class="container-xl px-0 px-md-3">
-	<h2 class="fs-3 m-4">最新の記事</h2>
+	<h2 class="fs-3 m-4 mt-5"><i class="bi-journals"></i> 最新の記事</h2>
 	<ul role="list" class="tile-articles list-unstyled d-lg-flex flex-wrap">
 		<?php
 		$query = tk_get_recent_posts();
